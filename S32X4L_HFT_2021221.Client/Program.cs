4ec088278763946b9cb047e95ae53e99947fd661
Bence Vass <vass.bence14@gmail.com>
2021-10-09 19:28:46 +0200

    class name fix

---
+++ S32X4L_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S32X4L_HFT_2021221.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- S32X4L_HFT_2021221.Endpoint/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace S32X4L_HFT_2021221.Endpoint
-{
-    public class Class1
-    {
-    }
-}
